0367164ec279b072be88fa603d1146af73081396
marcinmika <marcinmika@o2.pl>
2023-04-20 14:13:36 +0200

    Update index.js

--- serwer/serwer/index.js
+++ serwer/serwer/index.js
@@ -1,1 +1,20 @@
+const axios = require('axios');
 
+ const jsonUrl = 'http://192.168.88.254/cf/';
+
+      // Funkcja pobieraj ^eca dane z pliku JSON za pomoc ^e Axios i wy ^{wietlaj ^eca je na stronie
+      async function fetchData() {
+        try {
+          const response = await axios.get(jsonUrl);
+          const jsonData = response.data;
+          const jsonDataString = JSON.stringify(jsonData);
+                  const parsedData = JSON.parse(jsonDataString);
+                  //DevList();
+                  console.log(parsedData);
+          document.getElementById('json-data').textContent = jsonDataString;
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+ fetchData();
